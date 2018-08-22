ca970cec26239a266593b1cc4582e460495200da
Domash <35339409+Domash@users.noreply.github.com>
2018-08-22 19:02:26 +0300

    Add files via upload

--- lab7_audio_shop/main.c
+++ lab7_audio_shop/main.c
@@ -20,6 +20,7 @@ typedef struct Singer {
 } Singer;
 
 int get_int() {
+
     int res;
     while(true) {
         if(scanf("%d", &res) && getchar() == '\n') {
@@ -32,8 +33,10 @@ int get_int() {
 }
 
 bool string_comparison(const char *str_a, const char *str_b) {
+    
     size_t len_a = strlen(str_a);
     size_t len_b = strlen(str_b);
+    
     if(len_a != len_b) return false;
     for(int i = 0; i < len_a; ++i) {
         if(str_a[i] != str_b[i]) return false;
@@ -42,31 +45,40 @@ bool string_comparison(const char *str_a, const char *str_b) {
 }
 
 void clear_buff() {
+
     fflush(stdin);
     fflush(stdout);
 }
 
 void read_name(Singer *new_singer) {
+    
     printf("1.Enter singer's name\n");
     clear_buff();
     gets(buff);
+    
     size_t len = strlen(buff);
     new_singer->name = (char*)malloc(sizeof(char) * len);
+    
     for(int i = 0; i < len; ++i) {
         new_singer -> name[i] = buff[i];
     }
 }
 
 void read_albums(Singer *new_singer) {
+    
     clear_buff();
     printf("2.Numer of albums:\n");
+    
     int number_of_albums = get_int();
     new_singer -> number_of_albums = number_of_albums;
     new_singer -> list_of_albums = (char**)malloc(sizeof(char*) * number_of_albums);
+    
     for(int i = 0; i < number_of_albums; ++i) {
+        
         gets(buff);
         size_t len = strlen(buff);
         new_singer -> list_of_albums[i] = (char*)malloc(sizeof(char) * len);
+        
         for(int j = 0; j < len; ++j) {
             new_singer -> list_of_albums[i][j] = buff[j];
         }
@@ -74,15 +86,20 @@ void read_albums(Singer *new_singer) {
 }
 
 void read_tracks(Singer *new_singer) {
+
     clear_buff();
     printf("3.Numer of tracks:\n");
+    
     int number_of_tracks = get_int();
     new_singer -> flag = (bool*)malloc(sizeof(bool) * number_of_tracks);
+    
     for(int i = 0; i < number_of_tracks; ++i) {
         new_singer -> flag[i] = false;
     }
+    
     new_singer -> number_of_tracks = number_of_tracks;
     new_singer -> list_of_tracks = (char**)malloc(sizeof(char*) * number_of_tracks);
+    
     for(int i = 0; i < number_of_tracks; ++i) {
         gets(buff);
         size_t len = strlen(buff);
@@ -94,57 +111,74 @@ void read_tracks(Singer *new_singer) {
 }
 
 void read_information(Singer *new_singer) {
+    
     clear_buff();
-    puts("Info");
+    printf("Info");
     gets(buff);
+    
     size_t len = strlen(buff);
     new_singer -> information_abour_singer = (char*)malloc(sizeof(char) * len);
+    
     for(int i = 0; i < len; ++i) {
         new_singer -> information_abour_singer[i] = buff[i];
     }
 }
 
 void print_information(Singer *singer) {
+    
     printf("\nArtist: %s\n", singer -> name);
     printf("Info about artist: %s\n", singer -> information_abour_singer);
     printf("Number of albums = %d\n", singer -> number_of_albums);
+    
     for(int i = 0; i < singer -> number_of_albums; ++i) {
         printf("   %d.%s\n", i + 1, singer -> list_of_albums[i]);
     }
+    
     printf("Number of tracks = %d\n", singer -> number_of_tracks);
+    
     for(int i = 0; i < singer -> number_of_tracks; ++i) {
         printf("   %d.%s\n", i + 1, singer -> list_of_tracks[i]);
     }
+    
     printf("\n");
 }
 
 void search_singer_by_name(int *size_of_list_of_singers, Singer ***list_of_singers) {
+    
     clear_buff();
     printf("Enter singer's name\n");
+    
     gets(buff);
     size_t len = strlen(buff);
     char *name = (char*)malloc(sizeof(char) * len);
+    
     for(int i = 0; i < len; ++i) {
         name[i] = buff[i];
     }
+    
     for(int i = 0; i < *size_of_list_of_singers; ++i) {
         if(string_comparison(name, (*list_of_singers[i]) -> name) == true) {
             print_information(*list_of_singers[i]);
             return;
         }
     }
-    puts("Information is absent\n\n");
+    
+    printf("Information is absent\n\n");
 }
 
 void search_singer_by_track(int *size_of_list_of_singers, Singer ***list_of_singers) {
+    
     clear_buff();
     printf("Enter track\n");
+    
     gets(buff);
     size_t len = strlen(buff);
     char *name_of_track = (char*)malloc(sizeof(char) * len);
+    
     for(int i = 0; i < len; ++i) {
         name_of_track[i] = buff[i];
     }
+    
     for(int i = 0; i < *size_of_list_of_singers; ++i) {
         for(int j = 0; j < (*list_of_singers[i]) -> number_of_tracks; ++j) {
             if(string_comparison(name_of_track, (*list_of_singers[i]) -> list_of_tracks[j]) == true
@@ -154,18 +188,23 @@ void search_singer_by_track(int *size_of_list_of_singers, Singer ***list_of_sing
             }
         }
     }
-    puts("Information is absent\n\n");
+
+    printf("Information is absent\n\n");
 }
 
 void search_singer_by_album(int *size_of_list_of_singers, Singer ***list_of_singers) {
+    
     clear_buff();
     printf("Enter album\n");
+    
     gets(buff);
     size_t len = strlen(buff);
     char *name_of_album = (char*)malloc(sizeof(char) * len);
+    
     for(int i = 0; i < len; ++i) {
         name_of_album[i] = buff[i];
     }
+    
     for(int i = 0; i < *size_of_list_of_singers; ++i) {
         for(int j = 0; j < (*list_of_singers[i]) -> number_of_albums; ++j) {
             if(string_comparison(name_of_album, (*list_of_singers[i]) -> list_of_albums[j]) == true) {
@@ -174,10 +213,12 @@ void search_singer_by_album(int *size_of_list_of_singers, Singer ***list_of_sing
             }
         }
     }
-    puts("Information is absent\n\n");
+
+    printf("Information is absent\n\n");
 }
 
 void add_singer(int *size_of_list_of_singers, Singer ***list_of_singers) {
+    
     Singer *new_singer = (Singer*)malloc(sizeof(Singer));
     *list_of_singers = (Singer**)realloc(*list_of_singers, (*size_of_list_of_singers + 1) * sizeof(Singer*));
     
@@ -187,22 +228,22 @@ void add_singer(int *size_of_list_of_singers, Singer ***list_of_singers) {
     read_information(new_singer);
     
     *list_of_singers[*size_of_list_of_singers] = new_singer;
-    
-    printf("%s", (*list_of_singers[*size_of_list_of_singers]) -> name);
-    printf("\n");
-    
     *size_of_list_of_singers = *size_of_list_of_singers + 1;
 }
 
 void move_track_in_basket(int *size_of_list_of_singers, Singer ***list_of_singers) {
+    
     clear_buff();
     printf("Enter track\n");
+
     gets(buff);
     size_t len = strlen(buff);
     char *name_of_track = (char*)malloc(sizeof(char) * len);
+    
     for(int i = 0; i < len; ++i) {
         name_of_track[i] = buff[i];
     }
+
     for(int i = 0; i < *size_of_list_of_singers; ++i) {
         for(int j = 0; j < (*list_of_singers[i]) -> number_of_tracks; ++j) {
             if(string_comparison(name_of_track, (*list_of_singers[i]) -> list_of_tracks[j]) == true) {
@@ -210,18 +251,23 @@ void move_track_in_basket(int *size_of_list_of_singers, Singer ***list_of_singer
             }
         }
     }
+
     printf("\n");
 }
 
 void restore_track_from_basket(int *size_of_list_of_singers, Singer ***list_of_singers) {
+    
     clear_buff();
     printf("Enter track\n");
+    
     gets(buff);
     size_t len = strlen(buff);
     char *name_of_track = (char*)malloc(sizeof(char) * len);
+    
     for(int i = 0; i < len; ++i) {
         name_of_track[i] = buff[i];
     }
+
     for(int i = 0; i < *size_of_list_of_singers; ++i) {
         for(int j = 0; j < (*list_of_singers[i]) -> number_of_tracks; ++j) {
             if(string_comparison(name_of_track, (*list_of_singers[i]) -> list_of_tracks[j]) == true) {
@@ -229,19 +275,22 @@ void restore_track_from_basket(int *size_of_list_of_singers, Singer ***list_of_s
             }
         }
     }
+    printf("\n");
 }
 
 void get_report(int *size_of_list_of_singers, Singer ***list_of_singers) {
+    
     printf("\nCurrent status:\n");
     printf("   Number of artist: %d\n", *size_of_list_of_singers);
+
     int all_tracks = 0, all_albums = 0;
     for(int i = 0; i < *size_of_list_of_singers; ++i) {
         all_tracks += (*list_of_singers[i]) -> number_of_tracks;
         all_albums += (*list_of_singers[i]) -> number_of_albums;
     }
+
     printf("   Number of albums: %d\n", all_albums);
     printf("   Number of tracks: %d\n\n", all_tracks);
-    
 }
 
 void print_commands() {
@@ -259,6 +308,7 @@ void menu(int *size_of_list_of_singers, Singer ***list_of_singers) {
     
     bool ok = true;
     while(ok) {
+        
         print_commands();
         int command = get_int();
         
@@ -295,22 +345,28 @@ void menu(int *size_of_list_of_singers, Singer ***list_of_singers) {
 }
 
 void clear_memory(int *size_of_list_of_singers, Singer ***list_of_singers) {
+    
     for(int i = 0; i < *size_of_list_of_singers; ++i) {
+        
         for(int j = 0; j < (*list_of_singers[i]) -> number_of_albums; ++j) {
             free((*list_of_singers[i]) -> list_of_albums[j]);
         }
+
         for(int j = 0; j < (*list_of_singers[i]) -> number_of_tracks; ++j) {
             free((*list_of_singers[i]) -> list_of_tracks[j]);
         }
+
         free((*list_of_singers[i]) -> information_abour_singer);
         free((*list_of_singers[i]) -> flag);
         free(*list_of_singers[i]);
+    
     }
-    free(list_of_singers);
+    
+    free(*list_of_singers);
 }
 
-int main()
-{
+int main() {
+    
     Singer **list_of_singers = NULL;
     int size_of_list_of_singers = 0;
     menu(&size_of_list_of_singers, &list_of_singers);
